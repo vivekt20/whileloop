18f8e390721bc59020876905ab3e8eee8f424f10
fayas <muhammedfayas59504@gmail.com>
2024-08-08 09:17:21 +0530

    whileloop programs on 2/8/2024

---
+++ whileloop1.dart
@@ -0,0 +1,9 @@
+void main(){
+int sum=0;
+int i=1;
+while(i<=100){
+sum=sum+i;
+print(sum);
+i++;
+}
+}
---
+++ whileloop2.dart
@@ -0,0 +1,8 @@
+void main(){
+int i=1;
+while(i<=10){
+int square=i*i*i;
+print("square of $i is $square");
+i++;
+}
+}
